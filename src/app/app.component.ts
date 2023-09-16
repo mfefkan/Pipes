4eaaee6485d96702c507e4dd4f4accfd3999c4b8
fatih efkan <mfefkan@hotmail.com>
2023-09-16 14:05:41 +0300

    Pipe konusu örnekleri ile işlendi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CustomPipe } from './pipes/custom.pipe';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '{{name | slice :0:8 }} <br>{{"Muhabbet baldan tatlıdır, doyamazsın demedim mi ? " | custom:8:37}}                        <!--                                                                                                     1-  Pipes kullanırken | operatörünü yazdıktan sonra kullanacağımız pipe ı yazıyoruz.                                                                                                                                                                                                  2- "uppercase" Burada name parametresi içindeki string ifade büyük harfler ile template aktarılır. Böylelikle bir metni sadece uppercase pipe ını kullanarak görüntüsünü değiştirebiliriz. Bu pipe name parametresinin asıl değerini değiştirmez sadece frontende görüntü olarak işleyip gönderir.                                                                                                3- "slice" operatörü ile  array indis sayısı belirterek string ifadeyi indis numaralarından ayıklayarak iki indis arasındaki değeri yazdırır.                                                                                                4- "currency"   sayısal değerleri parasal formata dönüştürür.  bunu kullanmak için ya module kısmında provider"a deffault para birimini tanımlayabiliriz ya da şu şekilde -> "| currency : "$" "  her kullanımda belirterek de kullanabiliriz.                                                                                          5- "Date" Tarihsel verileri biçimlendirir.  örneğin : {{"23.05.1996"| date}} yaptığımızda sonucu May 23, 1996 olarak görebiliriz. Farklı kullanım varyasyonlarıda vardır. Onlarıda örneğin date: "fulldate"(medium,shortTime,mm:ss gibi) şeklinde kullanıp daha anlaşılır ve ihtiyaca yönelik olarak sunabiliriz.                                                                                             6-  "Json" Bir nesneyi ya da nesneleri json formatına dönüştürebilir.                                                                                          7-   "UpperCase" ve "LowerCase" metnin harflerini küçük ya da büyük gösterebiliriz.                                                                                          8-   "Percent" Sayısal değerleri %"lik olarak formatlar.                                                                                               9- "TitleCase" Her kelimenin ilk harfini büyük yazdırabiliriz.                                                                                          10- "KeyValue"  Elimizdeki bir key value formatındaki verinin hem keyine hem valuesine ulaşmamızı sağlayan bir pipe çeşitidir.                                                                                               Custom pipe terminalde (angular cli) ng g p ...name... ya da uzun haliyle ng generate pipe ...name... şeklinde tanımlanır.      -->'
+
+
+
 })
 export class AppComponent {
-  title = 'Pipes';
+
+  constructor(private custom: CustomPipe) {
+    console.log(custom.transform("Demedim mi????????", 0, 12))
+  }
+  name = 'Muhammed';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomPipe } from './pipes/custom.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide:DEFAULT_CURRENCY_CODE,useValue:"₺"}, CustomPipe]  // bu property sayesinde deffault para birimini ₺ olarak belirleriz.  birden fazla property tanımlanmış
+  ,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
